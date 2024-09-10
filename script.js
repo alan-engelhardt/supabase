a4cfd382f8b5e9803458230ff8dc17a4df478eb0
Alan <ale@kea.dk>
2024-09-10 17:55:46 +0200

    git ignore init.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const url = "https://jftyavgnjvzhcjchqdpg.supabase.co//rest/v1/TSL";
-
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmdHlhdmduanZ6aGNqY2hxZHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NTQ3MTIsImV4cCI6MjAyMzMzMDcxMn0.Tov11N_g-ERTctEqGBbYOr8FgVmDLWfB_-m1MbWcnEw";
-
 const options = {
   headers: {
     apikey: key,
